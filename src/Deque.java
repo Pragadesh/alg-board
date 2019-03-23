1fafd5ed44c75d38edec6c9feabb3173352b7446
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-03-23 07:50:30 -0700

    Replaced tabs

--- src/Deque.java
+++ src/Deque.java
@@ -1,148 +1,146 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-import edu.princeton.cs.algs4.StdOut;
-
 public class Deque<Item> implements Iterable<Item> {
 
-	private int capacity = 5;
-	private Item[] items;
+    private int capacity = 5;
+    private Item[] items;
 
-	private int first;
-	private int last;
+    private int first;
+    private int last;
 
-	// construct an empty deque
-	public Deque() {
-		items = (Item[]) new Object[capacity];
-		first = 1;
-		last = 1;
-	}
+    // construct an empty deque
+    public Deque() {
+        items = (Item[]) new Object[capacity];
+        first = 1;
+        last = 1;
+    }
 
-	// is the deque empty?
-	public boolean isEmpty() {
-		return (size() == 0);
-	}
+    // is the deque empty?
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
 
-	// return the number of items on the deque
-	public int size() {
-		return (last + items.length - first) % items.length;
-	}
+    // return the number of items on the deque
+    public int size() {
+        return (last + items.length - first) % items.length;
+    }
 
-	// add the item to the front
-	public void addFirst(Item item) {
-		if (item == null) {
-			throw new NoSuchElementException();
-		}
-		checkArrayCapacity();
-		if (first == 0) {
-			first = items.length;
-		}
-		items[--first] = item;
-	}
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new NoSuchElementException();
+        }
+        checkArrayCapacity();
+        if (first == 0) {
+            first = items.length;
+        }
+        items[--first] = item;
+    }
 
-	// add the item to the end
-	public void addLast(Item item) {
-		if (item == null) {
-			throw new NoSuchElementException();
-		}
-		checkArrayCapacity();
-		items[last] = item;
-		last = (last + 1) % items.length;
-	}
+    // add the item to the end
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new NoSuchElementException();
+        }
+        checkArrayCapacity();
+        items[last] = item;
+        last = (last + 1) % items.length;
+    }
 
-	// remove and return the item from the front
-	public Item removeFirst() {
-		if (first == last) {
-			throw new NoSuchElementException();
-		}
-		Item item = items[first];
-		items[first++] = null;
-		first = first % items.length;
-		shrinkIfRequired();
-		return item;
-	}
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (first == last) {
+            throw new NoSuchElementException();
+        }
+        Item item = items[first];
+        items[first++] = null;
+        first = first % items.length;
+        shrinkIfRequired();
+        return item;
+    }
 
-	// remove and return the item from the end
-	public Item removeLast() {
-		if (first == last) {
-			throw new NoSuchElementException();
-		}
-		last = (last + items.length - 1) % items.length;
-		Item item = items[last];
-		items[last] = null;
-		shrinkIfRequired();
-		return item;
-	}
+    // remove and return the item from the end
+    public Item removeLast() {
+        if (first == last) {
+            throw new NoSuchElementException();
+        }
+        last = (last + items.length - 1) % items.length;
+        Item item = items[last];
+        items[last] = null;
+        shrinkIfRequired();
+        return item;
+    }
 
-	// return an iterator over items in order from front to end
-	public Iterator<Item> iterator() {
-		return new DequeItr();
-	}
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return new DequeItr();
+    }
 
-	private boolean isFull() {
-		return (size() >= items.length - 1);
-	}
+    private boolean isFull() {
+        return (size() >= items.length - 1);
+    }
 
-	private void modifyCapacity(int newCapacity) {
-		Item[] newItems = (Item[]) new Object[newCapacity];
-		for (int i = 0; i < size(); i++) {
-			newItems[i] = items[(first + i) % items.length];
-		}
-		this.last = size();
-		this.first = 0;
-		this.items = newItems;
-		this.capacity = newCapacity;
-	}
+    private void modifyCapacity(int newCapacity) {
+        Item[] newItems = (Item[]) new Object[newCapacity];
+        for (int i = 0; i < size(); i++) {
+            newItems[i] = items[(first + i) % items.length];
+        }
+        this.last = size();
+        this.first = 0;
+        this.items = newItems;
+        this.capacity = newCapacity;
+    }
 
-	private void checkArrayCapacity() {
-		if (isFull()) {
-			modifyCapacity(capacity * 2);
-		}
-	}
+    private void checkArrayCapacity() {
+        if (isFull()) {
+            modifyCapacity(capacity * 2);
+        }
+    }
 
-	private void shrinkIfRequired() {
-		if (size() <= items.length / 4) {
-			modifyCapacity(capacity / 2);
-		}
-	}
+    private void shrinkIfRequired() {
+        if (size() <= items.length / 4) {
+            modifyCapacity(capacity / 2);
+        }
+    }
 
-	private class DequeItr implements Iterator<Item> {
+    private class DequeItr implements Iterator<Item> {
 
-		private int start;
+        private int start;
 
-		public DequeItr() {
-			this.start = first;
-		}
+        public DequeItr() {
+            this.start = first;
+        }
 
-		@Override
-		public boolean hasNext() {
-			return (start != last);
-		}
+        @Override
+        public boolean hasNext() {
+            return (start != last);
+        }
 
-		@Override
-		public Item next() {
-			if (start == last) {
-				throw new NoSuchElementException();
-			}
-			Item item = items[start++];
-			start = start % items.length;
-			return item;
-		}
+        @Override
+        public Item next() {
+            if (start == last) {
+                throw new NoSuchElementException();
+            }
+            Item item = items[start++];
+            start = start % items.length;
+            return item;
+        }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-	}
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
-	// unit testing (optional)
-	public static void main(String[] args) {
-		Deque<String> deque = new Deque<>();
-		deque.addFirst("A");
-		deque.addFirst("Z");
-		deque.addLast("B");
-		deque.removeFirst();
+    // unit testing (optional)
+    public static void main(String[] args) {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("A");
+        deque.addFirst("Z");
+        deque.addLast("B");
+        deque.removeFirst();
 
-	}
+    }
 
 }
--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -6,122 +6,122 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	private int capacity = 5;
-	private Item[] items;
+    private int capacity = 5;
+    private Item[] items;
 
-	private int size;
+    private int size;
 
-	// construct an empty randomized queue
-	public RandomizedQueue() {
-		this.items = (Item[]) new Object[capacity];
-	}
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        this.items = (Item[]) new Object[capacity];
+    }
 
-	// is the randomized queue empty?
-	public boolean isEmpty() {
-		return (size == 0);
-	}
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+        return (size == 0);
+    }
 
-	// return the number of items on the randomized queue
-	public int size() {
-		return size;
-	}
+    // return the number of items on the randomized queue
+    public int size() {
+        return size;
+    }
 
-	// add the item
-	public void enqueue(Item item) {
-		if (item == null) {
-			throw new NoSuchElementException();
-		}
-		checkArrayCapacity();
-		items[size++] = item;
-	}
+    // add the item
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new NoSuchElementException();
+        }
+        checkArrayCapacity();
+        items[size++] = item;
+    }
 
-	// remove and return a random item
-	public Item dequeue() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		shrinkIfRequired();
-		int rdmIdx = StdRandom.uniform(size);
-		Item item = items[rdmIdx];
-		if (rdmIdx != size - 1) {
-			items[rdmIdx] = items[size - 1];
-		}
-		items[--size] = null;
-		return item;
-	}
+    // remove and return a random item
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        shrinkIfRequired();
+        int rdmIdx = StdRandom.uniform(size);
+        Item item = items[rdmIdx];
+        if (rdmIdx != size - 1) {
+            items[rdmIdx] = items[size - 1];
+        }
+        items[--size] = null;
+        return item;
+    }
 
-	// return a random item (but do not remove it)
-	public Item sample() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		int rdmIdx = StdRandom.uniform(size);
-		return items[rdmIdx];
-	}
+    // return a random item (but do not remove it)
+    public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        int rdmIdx = StdRandom.uniform(size);
+        return items[rdmIdx];
+    }
 
-	// return an independent iterator over items in random order
-	@Override
-	public Iterator<Item> iterator() {
-		return new RandomizedQueueItr();
-	}
+    // return an independent iterator over items in random order
+    @Override
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueItr();
+    }
 
-	private void modifyCapacity(int newCapacity) {
-		Item[] newItems = (Item[]) new Object[newCapacity];
-		for (int i = 0; i < size(); i++) {
-			newItems[i] = items[i];
-		}
-		this.items = newItems;
-		this.capacity = newCapacity;
-	}
+    private void modifyCapacity(int newCapacity) {
+        Item[] newItems = (Item[]) new Object[newCapacity];
+        for (int i = 0; i < size(); i++) {
+            newItems[i] = items[i];
+        }
+        this.items = newItems;
+        this.capacity = newCapacity;
+    }
 
-	private void checkArrayCapacity() {
-		if (size() == items.length) {
-			modifyCapacity(capacity * 2);
-		}
-	}
+    private void checkArrayCapacity() {
+        if (size() == items.length) {
+            modifyCapacity(capacity * 2);
+        }
+    }
 
-	private void shrinkIfRequired() {
-		if (size() <= items.length / 4) {
-			modifyCapacity(capacity / 2);
-		}
-	}
+    private void shrinkIfRequired() {
+        if (size() <= items.length / 4) {
+            modifyCapacity(capacity / 2);
+        }
+    }
 
-	private class RandomizedQueueItr implements Iterator<Item> {
+    private class RandomizedQueueItr implements Iterator<Item> {
 
-		private final int[] shuffledIndex;
-		private int currentIdx;
+        private final int[] shuffledIndex;
+        private int currentIdx;
 
-		public RandomizedQueueItr() {
-			this.shuffledIndex = new int[size];
-			for (int i = 0; i < size; i++) {
-				shuffledIndex[i] = i;
-			}
-			StdRandom.shuffle(shuffledIndex);
-		}
+        public RandomizedQueueItr() {
+            this.shuffledIndex = new int[size];
+            for (int i = 0; i < size; i++) {
+                shuffledIndex[i] = i;
+            }
+            StdRandom.shuffle(shuffledIndex);
+        }
 
-		@Override
-		public boolean hasNext() {
-			return (currentIdx < shuffledIndex.length);
-		}
+        @Override
+        public boolean hasNext() {
+            return (currentIdx < shuffledIndex.length);
+        }
 
-		@Override
-		public Item next() {
-			if (currentIdx >= shuffledIndex.length) {
-				throw new NoSuchElementException();
-			}
-			return items[shuffledIndex[currentIdx++]];
-		}
+        @Override
+        public Item next() {
+            if (currentIdx >= shuffledIndex.length) {
+                throw new NoSuchElementException();
+            }
+            return items[shuffledIndex[currentIdx++]];
+        }
 
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
 
-	}
+    }
 
-	// unit testing (optional)
-	public static void main(String[] args) {
-		RandomizedQueue<String> rdmQueue = new RandomizedQueue<>();
-		rdmQueue.enqueue("A");
-	}
+    // unit testing (optional)
+    public static void main(String[] args) {
+        RandomizedQueue<String> rdmQueue = new RandomizedQueue<>();
+        rdmQueue.enqueue("A");
+    }
 }
