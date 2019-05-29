a6bdc2cfb86e8e79d52d53b0eb8d224b83b7202a
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-05-29 08:38:31 -0700

    week 5 assignment

---
+++ src/KdTree.java
@@ -0,0 +1,242 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class KdTree {
+	private Node root;
+	private int maxLevel;
+
+	public KdTree() {
+		// construct an empty set of points
+		maxLevel = 2;
+	}
+
+	public boolean isEmpty() {
+		// is the set empty?
+		return (size() == 0);
+	}
+
+	private int size(Node node) {
+		if (node != null) {
+			return node.size;
+		}
+		return 0;
+	}
+
+	public int size() {
+		// number of points in the set
+		return size(root);
+	}
+
+	public void insert(Point2D p) {
+		// add the point to the set (if it is not already in the set)
+		if (p == null) {
+			throw new IllegalArgumentException("Empty point to insert");
+		}
+		root = insert(root, p, -1);
+	}
+
+	private Node insert(Node node, Point2D point, int level) {
+		if (node == null) {
+			int currLevel = getNextLevel(level);
+			return new Node(point, currLevel);
+		}
+		int compareValue = getComparator(node.level).compare(point, node.key);
+		if (compareValue < 0) {
+			node.left = insert(node.left, point, node.level);
+		} else if (compareValue > 0) {
+			node.right = insert(node.right, point, node.level);
+		} else {
+			node.values.add(point);
+		}
+		node.size = size(node.left) + size(node.right) + node.values.size();
+		return node;
+	}
+
+	private Comparator<Point2D> getComparator(int level) {
+		if (level == 0) {
+			return Point2D.X_ORDER;
+		}
+		return Point2D.Y_ORDER;
+	}
+
+	private int getNextLevel(int level) {
+		if (level < 0) {
+			return 0;
+		}
+		return (++level) % maxLevel;
+	}
+
+	public boolean contains(Point2D p) {
+		// does the set contain point p?
+		if (p == null) {
+			throw new IllegalArgumentException("Empty point to find");
+		}
+		return contains(root, p);
+	}
+
+	private boolean contains(Node node, Point2D point) {
+		if (node == null) {
+			return false;
+		}
+		int compareValue = getComparator(node.level).compare(point, node.key);
+		if (compareValue < 0) {
+			return contains(node.left, point);
+		} else if (compareValue > 0) {
+			return contains(node.right, point);
+		} else {
+			for (Point2D p : node.values) {
+				if (p.equals(point)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	public void draw() {
+		// draw all points to standard draw
+		List<Point2D> points = new ArrayList<>();
+		StdDraw.setScale();
+		inOrder(root, points);
+		for (Point2D point : points) {
+			StdDraw.point(point.x(), point.y());
+		}
+
+	}
+
+	private void inOrder(Node node, List<Point2D> points) {
+		if (node == null) {
+			return;
+		}
+		inOrder(node.left, points);
+		for (Point2D p : node.values) {
+			points.add(p);
+		}
+		inOrder(node.right, points);
+	}
+
+	public Iterable<Point2D> range(RectHV rect) {
+		// all points that are inside the rectangle (or on the boundary)
+		List<Point2D> pointsInRange = new ArrayList<>();
+		range(rect, root, pointsInRange);
+		return pointsInRange;
+	}
+
+	private void range(RectHV rect, Node node, List<Point2D> pointsInRange) {
+		if (node == null) {
+			return;
+		}
+		double lower = rect.xmin();
+		double upper = rect.xmax();
+		double value = node.key.x();
+		if (node.level == 1) {
+			lower = rect.ymin();
+			upper = rect.ymax();
+			value = node.key.y();
+		}
+		if (Double.compare(value, lower) < 0) {
+			range(rect, node.left, pointsInRange);
+		} else if (Double.compare(value, upper) > 0) {
+			range(rect, node.right, pointsInRange);
+		} else {
+			range(rect, node.left, pointsInRange);
+			for (Point2D p : node.values) {
+				if (rect.contains(p)) {
+					pointsInRange.add(p);
+				}
+			}
+			range(rect, node.right, pointsInRange);
+		}
+	}
+
+	public Point2D nearest(Point2D p) {
+		// a nearest neighbor in the set to point p; null if the set is empty
+		return nearest(p, root, null);
+	}
+	
+	private Point2D nearest(Point2D p, Node node, Point2D nearest) {
+		if(node == null) {
+			return nearest;
+		}
+		Comparator<Point2D> comparator = p.distanceToOrder();
+		int compare = node.level == 0? Double.compare(p.x(), node.key.x()) : Double.compare(p.y(), node.key.y());
+		if(compare < 0) {
+			Point2D leftNearest = nearest(p, node.left, nearest);
+			if(nearest == null || comparator.compare(leftNearest, nearest) < 0) {
+				nearest = leftNearest;
+			}
+		}else {
+			Point2D rightNearest = nearest(p, node.right, nearest);
+			if(nearest == null || comparator.compare(rightNearest, nearest) < 0) {
+				nearest = rightNearest;
+			}
+		}
+		if(nearest == null) {
+			nearest = node.key;
+		}
+		double axisDistance = node.level == 0? Math.abs(node.key.x() - p.x()) : Math.abs(node.key.y() - p.y());
+		if(Double.compare(p.distanceTo(nearest), axisDistance) >= 0) {
+			nearest = findNearestPoint(node, p, nearest);
+			if(compare < 0) {
+				nearest = nearest(p, node.right, nearest);
+			}else {
+				nearest = nearest(p, node.left, nearest);
+			}
+		}
+		return nearest;
+	}
+	
+	private Point2D findNearestPoint(Node node, Point2D p, Point2D nearest) {
+		Comparator<Point2D> comparator = p.distanceToOrder();
+		for(Point2D point : node.values) {
+			if(nearest == null || comparator.compare(point, nearest) < 0) {
+				nearest = point;
+			}
+		}
+		return nearest;
+	}
+
+	private static class Node {
+		private Point2D key;
+		private List<Point2D> values;
+		private int size;
+		private int level;
+		private Node left;
+		private Node right;
+
+		public Node(Point2D key, int level) {
+			this.key = key;
+			this.values = new ArrayList<>();
+			this.values.add(key);
+			this.level = level;
+			this.size = 1;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		KdTree tree = new KdTree();
+		PointSET pointSet = new PointSET();
+		addPoint(tree, pointSet, new Point2D(1, 1));
+		addPoint(tree, pointSet, new Point2D(2, 3));
+		addPoint(tree, pointSet, new Point2D(0, 1));
+		addPoint(tree, pointSet, new Point2D(0, 5));
+		addPoint(tree, pointSet, new Point2D(1, 3));
+		RectHV rect = new RectHV(0, 0, 2, 2);
+		System.out.println("KD Tree nearest: " + tree.nearest(new Point2D(0, 0))) ;
+		System.out.println("pointSet nearest: " + pointSet.nearest(new Point2D(0, 0))) ;
+		
+		System.out.println("KD Tree range: " + tree.range(rect)) ;
+		System.out.println("pointSet range: " + pointSet.range(rect)) ;
+	}
+	
+	private static void addPoint(KdTree tree, PointSET pointSet, Point2D point) {
+	    tree.insert(point);
+	    pointSet.insert(point);
+	}
+}
