d4a43f8d507d0166205d49dbb1beb0b0532fbbd6
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-10-17 05:46:25 -0700

    Burrows wheeler setup

---
+++ src/CircularSuffixArray.java
@@ -0,0 +1,26 @@
+
+/*
+ * https://coursera.cs.princeton.edu/algs4/assignments/burrows/specification.php
+ */
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+        
+    }
+
+    // length of s
+    public int length() {
+        return -1;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return -1;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/MoveToFront.java
@@ -0,0 +1,22 @@
+
+/*
+ * https://coursera.cs.princeton.edu/algs4/assignments/burrows/specification.php
+ */
+public class MoveToFront {
+
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+        
+    }
+
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+        
+    }
+
+    // if args[0] is "-", apply move-to-front encoding
+    // if args[0] is "+", apply move-to-front decoding
+    public static void main(String[] args) {
+        
+    }
+}
